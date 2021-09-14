ac2d94166f45f0816861e4a14c5a69082b80503b
Hector Villasano <hectorstevenvillasano@gmail.com>
2021-09-13 23:20:31 -0700

    move

--- Mini_Servo_robot_arm/Mini_Servo_robot_arm.ino
+++ Mini_Servo_robot_arm/Mini_Servo_robot_arm.ino
@@ -9,27 +9,27 @@
 
 #include <Servo.h>
 
-#define delayTime 50
-#define servoCount 4
+#define DELAYTIME 50
+#define SERVOCOUNT 4
 
 // servo pin numbers
-#define servo0Pin 7
-#define servo1Pin 8
-#define servo2Pin 9
-#define servo3Pin 10
+#define S0PIN 7
+#define S1PIN 8
+#define S2PIN 9
+#define S3PIN 10
 
 // servo min max anggles
-#define servo0Min 0
-#define servo0Max 175
+#define S0MIN 0
+#define S0MAX 180
 
-#define servo1Min 60
-#define servo1Max 100
+#define S1MIN 60
+#define S1MAX 180
 
-#define servo2Min 60
-#define servo2Max 180
+#define S2MIN 0
+#define S2MAX 180
 
-#define servo3Min 0
-#define servo3Max 180
+#define S3MIN 0
+#define S3MAX 180
 
 #define RESETANGLE 90
 
@@ -39,15 +39,15 @@ Servo servo2;
 Servo servo3;
 
 Servo servoList[] = {servo0, servo1, servo2, servo3};
-int servoMinList[] = {servo0Min, servo1Min, servo2Min, servo3Min};
-int servoMaxList[] = {servo0Max, servo1Max, servo2Max, servo3Max};
+int servoMinList[] = {S0MIN, S1MIN, S2MIN, S3MIN};
+int servoMaxList[] = {S0MAX, S1MAX, S2MAX, S3MAX};
 
 void setup() {
   Serial.begin(9600);
-  servo0.attach(servo0Pin);
-  servo1.attach(servo1Pin);
-  servo2.attach(servo2Pin);
-  servo3.attach(servo3Pin);
+  servo0.attach(S0PIN);
+  servo1.attach(S1PIN);
+  servo2.attach(S2PIN);
+  servo3.attach(S3PIN);
   delay(1000);
 }
 
@@ -57,6 +57,8 @@ void loop() {
   if (state == 0) {
     commandLineState();
   }
+
+  delay(DELAYTIME);
 }
 
 // command line state
@@ -72,31 +74,26 @@ void commandLineState() {
       String str = Serial.readString();
 
       int servoId = String(str[1]).toInt();
-      if ((str[0] == 's') && (servoId < servoCount)) {
+      if ((str[0] == 's') && (servoId < SERVOCOUNT)) {
         Servo servo = servoList[servoId];
         int intStart = str[3] == '+' || str[3] == '-' ? 4 : 3;
-
         String valueStr = "";
+
         for (int i = intStart; i < str.length(); i++) {
           valueStr += str[i];
         }
 
         int value = valueStr.toInt();
-        int angle = servo.read();
 
-        if (str[3] == '+') {
-          int newAngle = angle + value;
-          value = angleIsInRange(servoId, newAngle) == true ? newAngle : angle;
-        } else if(str[3] == '-') {
-          int newAngle = angle - value;
-          value = angleIsInRange(servoId, newAngle) == true ? newAngle : angle;
-          servo.write(value);
+        if (str[3] == '+' || str[3] == '-') {
+          value = move(servoId, String(str[3]), value);
         } else {
-          servo.write(value);
+          value = move(servoId, value);
         }
 
-        String message = "servo " + String(servoId) + " to angle " + String(value);
+        String message = "servo " + String(servoId) + " to angle " + String(servo.read());
         Serial.println(message);
+
       }else if (str[0] == 'p' && str[1] == 'a') {
         readServoAngles();
       } else if (str[0] == 'r' && str[1] == 's') {
@@ -105,19 +102,43 @@ void commandLineState() {
     }
 }
 
+int move(int servoId, int value) {
+  Servo servo = servoList[servoId];
+  int currentAngle = servo.read();
+  int newValue = angleIsInRange(servoId, value) == true ? value : currentAngle;
+  servo.write(newValue);
+  return newValue;
+}
+
+int move(int servoId, String op, int value) {
+  Servo servo = servoList[servoId];
+  int angle = servoList[servoId].read();
+
+  if (op == '+') {
+    int newAngle = angle + value;
+    value = angleIsInRange(servoId, newAngle) == true ? newAngle : angle;
+  } else if(op == '-') {
+    int newAngle = angle - value;
+    value = angleIsInRange(servoId, newAngle) == true ? newAngle : angle;
+  }
+
+  servo.write(value);
+  return value;
+}
+
 bool angleIsInRange(int servoId, int angle) {
-  return (servoMinList[servoId] >= angle && servoMaxList[servoId] <= angle) ? true : false;
+  return (angle >= servoMinList[servoId] && angle <= servoMaxList[servoId]) ? true : false;
 }
 
 int resetPosition() {
-  for (int i = 0; i < servoCount; i++) { servoList[i].write(RESETANGLE); }
+  for (int i = 0; i < SERVOCOUNT; i++) { servoList[i].write(RESETANGLE); }
   Serial.println("Reseting Servos to angle " + String(RESETANGLE));
 }
 
 int *readServoAngles() {
   int arr[4] = {};
 
-  for (int i = 0; i < servoCount; i++) {
+  for (int i = 0; i < SERVOCOUNT; i++) {
     arr[i] = servoList[i].read();
   }
 
@@ -126,19 +147,19 @@ int *readServoAngles() {
   return arr;
 }
 
-void swingServo(Servo servo, int maxPosition) {
-  for (int i = 0; i <  maxPosition; i+=5) {
-    servo.write(i);
-//    Serial.println(i);
-    delay(delayTime);
-  }
-
-  for (int i = maxPosition; i >= 0; i-=5) {
-    servo.write(i);
-//    Serial.println(i);
-    delay(delayTime);
-  }
-}
+// void swingServo(Servo servo, int maxPosition) {
+//   for (int i = 0; i <  maxPosition; i+=5) {
+//     servo.write(i);
+// //    Serial.println(i);
+//     delay(delayTime);
+//   }
+//
+//   for (int i = maxPosition; i >= 0; i-=5) {
+//     servo.write(i);
+// //    Serial.println(i);
+//     delay(delayTime);
+//   }
+// }
 
 void goToZero(Servo servo) {
   while (servo.read() > 0) {
@@ -153,6 +174,6 @@ void goToZero(Servo servo) {
       pinPosition = 0;
     }
 
-    delay(delayTime);
+    delay(DELAYTIME);
   }
 }
